606a8064acd0a10ad586ce7c6f0b91ae092d9448
Tshegofatso Buthane <BoipeloButhane@gmail.com>
2024-10-18 23:04:51 +0200

    updated

--- firewall.tf
+++ firewall.tf
@@ -7,7 +7,7 @@ resource "google_compute_firewall" "serverpod-instance" {
   source_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
 
   allow {
-    protocol = "http"
+    protocol = "tcp"
     ports    = ["8080-8082"]
   }
 
@@ -23,7 +23,7 @@ resource "google_compute_firewall" "serverpod-instance-ssh" {
   source_ranges = ["0.0.0.0/0"]
 
   allow {
-    protocol = "http"
+    protocol = "tcp"
     ports    = ["22"]
   }
 
